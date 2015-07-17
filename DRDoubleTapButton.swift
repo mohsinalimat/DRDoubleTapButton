bb537937fe95409dbca9288d1bdbb30d48c49f1e
Diego Rueda <contacto@diegorueda.es>
2015-07-17 18:00:56 +0200

    Initial commit

---
+++ DRDoubleTapButton.swift
@@ -0,0 +1,14 @@
+//
+//  DRDoubleTapButton.swift
+//  DRDoubleTapButton
+//
+//  Created by Diego Rueda on 17/07/15.
+//  Copyright (c) 2015 Diego Rueda. All rights reserved.
+//
+
+import UIKit
+
+class DRDoubleTapButton: UIView {
+
+
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# DRDoubleTapButton
+
+A button in two steps.
+
+Custom component for iOS that requires two taps: the first one to activate it and the second one to confirm the action.
+
